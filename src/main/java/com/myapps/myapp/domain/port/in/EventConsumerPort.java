d8eed640b3040267023594ce4a97ffce4c1b29b5
knoyz <kennynoy98@gmail.com>
2025-06-15 10:40:36 +0200

    feat: Add Javadoc comments for consumeProductDetailsChangedEvent method in EventConsumerPort interface

--- src/main/java/com/myapps/myapp/domain/port/in/EventConsumerPort.java
+++ src/main/java/com/myapps/myapp/domain/port/in/EventConsumerPort.java
@@ -4,6 +4,12 @@ import com.myapps.myapp.domain.events.ProductDetailsChangedEvent;
 
 public interface EventConsumerPort {
 
+    /**
+     * Changes the product details in the cache based on the provided product
+     * details changed event.
+     *
+     * @param event The product details changed event to be consumed.
+     */
     void consumeProductDetailsChangedEvent(ProductDetailsChangedEvent event);
 
 }
